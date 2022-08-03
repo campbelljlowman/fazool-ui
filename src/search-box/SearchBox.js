4c9c4f272b72ea927ac7a0fe93f97ab4c32dafeb
Campbell Lowman <campbelljlowman@gmail.com>
2022-08-03 18:48:34 -0400

    save commit for spotify client token

--- src/search-box/SearchBox.js
+++ src/search-box/SearchBox.js
@@ -1,11 +1,22 @@
-import React,  { useState } from 'react'
+import React,  { useState, useEffect } from 'react'
 
 function SearchBox () {
     const [searchValue, setSearchValue] = useState();
 
     const handleChange = (e) => {
-        setSearchValue(e.target.value)
+        setSearchValue(e.target.value);
     }
+
+    const getClientToken = () => {
+        console.log("Getting client token");
+    }
+
+    useEffect(getClientToken, []);
+
+    // Create funciton to get spotify client access token
+    // On startup, check for access token in local storage
+    // If no token, use function to get token, store in local storage and state
+    // If token in localstorage, check it's valid. If not, refresh
 
     return(
         <form>
