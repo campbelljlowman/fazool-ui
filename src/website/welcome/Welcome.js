cd4e3d8096982579d3ae80dbe8ba8b7ec112cab3
Campbell Lowman <campbelljlowman@gmail.com>
2022-09-09 16:00:55 -0400

    basic welcome page with nav buttons

--- src/website/welcome/Welcome.js
+++ src/website/welcome/Welcome.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import './Welcome.css'
+import { Container, Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 function Welcome() {
   return (
-    <div>Welcome</div>
+        <Container>
+        <Row className="justify-content-md-center">
+          <Col xs={8}>
+                <div className='nav-bar'>
+                    <div>Logo</div>
+                    <Link className='nav-button' to='/join'>Join Session</Link>
+                    <Link className='nav-button' to='/register'>Sign Up!</Link>
+                    <Link className='nav-button' to='/login'>Login</Link>
+                </div>
+                <div>Welcome</div>
+          </Col>
+        </Row>
+      </Container>
   )
 }
 
-export default Welcome
+export default Welcome; 
