ed905ecd0b1f92839309d5bc127e74b95c22ee19
Campbell Lowman <campbelljlowman@gmail.com>
2022-10-13 14:48:01 -0400

    save commit (not working)

--- src/session/search-box/SearchBox.js
+++ src/session/search-box/SearchBox.js
@@ -3,12 +3,12 @@ import {Buffer} from 'buffer';
 import SearchResults from './SearchResults';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
-
 import './SearchBox.css'
 
+const spotifyClientId = "a7666d8987c7487b8c8f345126bd1f0c";
+const spotifyClientSecret = "efa8b45e4d994eaebc25377afc5a9e8d";
+
 function SearchBox () {
-    const spotifyClientId = "a7666d8987c7487b8c8f345126bd1f0c";
-    const spotifyClientSecret = "efa8b45e4d994eaebc25377afc5a9e8d";
     const [searchValue, setSearchValue] = useState("");
     const [spotifyAccessToken, setSpotifyAccessToken] = useState();
     const [searchResults, setSearchResults] = useState();
--- src/website/callback/SpotifyCallback.js
+++ src/website/callback/SpotifyCallback.js
@@ -1,14 +1,48 @@
 import React from 'react'
 import { useSearchParams } from "react-router-dom";
- 
+import { useEffect } from 'react';
+
+const spotifyClientId = "a7666d8987c7487b8c8f345126bd1f0c";
+const spotifyClientSecret = "efa8b45e4d994eaebc25377afc5a9e8d";
+
 function SpotifyCallback() {
   const [searchParams] = useSearchParams();
+  const spotifyCode = searchParams.get("code")
 
-  console.log(searchParams)
-  console.log(searchParams.get("code"))
-  
+  if (!spotifyCode) {
+    console.log("Spotify authorization failed!")
+    const error = searchParams.get("Error")
+    console.log(error)
+  }
+
+  useEffect(() => {
+    const processCredentials = async () => {
+      const requestOptions = {
+        method: "Post",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            "Authorization": "Basic " + Buffer.from(`${spotifyClientId}:${spotifyClientSecret}`).toString("base64")
+        },
+        body: "grant_type=authorization_code",
+        code: spotifyCode
+      }
+      const rsp = await fetch("https://accounts.spotify.com/api/token", requestOptions);
+      if(!rsp.ok){
+          console.log(`Request error body: ${await rsp.text()}`);
+      } else{
+        const rspJson = await rsp.json();
+        // setSpotifyAccessToken(rspJson.access_token);
+        console.log(rspJson);
+    }
+
+    };
+    processCredentials();
+  }, []);
+
+
+
   return (
-    <div>SpotifyCallback</div>
+    <div>Saving Credentials</div>
   )
 }
 
