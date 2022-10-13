562dcd8815dd0ad6f0043c1615d72374708bbbae
Campbell Lowman <campbelljlowman@gmail.com>
2022-10-13 15:45:04 -0400

    get access token from spotify

--- src/website/callback/SpotifyCallback.js
+++ src/website/callback/SpotifyCallback.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Buffer} from 'buffer';
 import { useSearchParams } from "react-router-dom";
 import { useEffect } from 'react';
 
 const spotifyClientId = "a7666d8987c7487b8c8f345126bd1f0c";
 const spotifyClientSecret = "efa8b45e4d994eaebc25377afc5a9e8d";
+const redirectURI = 'http://localhost:3000/callback'
 
 function SpotifyCallback() {
   const [searchParams] = useSearchParams();
@@ -23,8 +25,11 @@ function SpotifyCallback() {
             'Content-Type': 'application/x-www-form-urlencoded',
             "Authorization": "Basic " + Buffer.from(`${spotifyClientId}:${spotifyClientSecret}`).toString("base64")
         },
-        body: "grant_type=authorization_code",
-        code: spotifyCode
+        body: new URLSearchParams ({
+          'grant_type': 'authorization_code',
+          'code': spotifyCode,
+          'redirect_uri': redirectURI
+        })
       }
       const rsp = await fetch("https://accounts.spotify.com/api/token", requestOptions);
       if(!rsp.ok){
@@ -35,9 +40,10 @@ function SpotifyCallback() {
         console.log(rspJson);
     }
 
+    // Redirect back to home
     };
     processCredentials();
-  }, []);
+  });
 
 
 
