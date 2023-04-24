fc821eb89de622f9c8c7d47700509e0cfc1c8510
Campbell Lowman <campbelllowman@gmail.com>
2023-04-24 16:31:28 -0400

    save commit

--- src/session/music-player/Song.tsx
+++ src/session/music-player/Song.tsx
@@ -5,9 +5,17 @@ interface SongProps {
     song: SimpleSong
 }
 function Song({ song }: SongProps) {
-  return (
-    <div className='cp-song'>Song</div>
-  )
+    if (!song) {
+        return null;
+    }
+
+    return (
+        <div className="cp-song">
+            <img className="cp-album-cover" src={song.image} alt="Album Cover"  />
+            <div className="cp-song-title">{song.title}</div>
+            <div className="cp-song-artist">{song.artist}</div>
+        </div>
+    );
 }
 
 export default Song
