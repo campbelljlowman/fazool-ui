be0a3e850d219133978465a951d7a76efcfd0900
Campbell Lowman <campbelllowman@gmail.com>
2023-05-01 20:05:03 -0400

    Add circle for number of votes

--- src/session/queue/QueueItem.tsx
+++ src/session/queue/QueueItem.tsx
@@ -75,8 +75,8 @@ function QueueItem({ queuedSong, sessionID, showDecrement, upVotedFor, downVoted
             <img className="queue-item-album-cover" src={queuedSong.simpleSong.image} alt="Album Cover"  />
             <div className="queue-item-song-title">{queuedSong.simpleSong.title}</div>
             <div className="queue-item-song-artist">{queuedSong.simpleSong.artist}</div>
-            {/* <div className='vote'> {queuedSong.votes} </div>
-            <div className="voter">
+            <div className='votes'>{queuedSong.votes}</div>
+            {/* <div className="voter">
                 {upvote()}
                 {downVote()}
             </div> */}
