eb69a24acd6fd77e42332f7cee2c8a011d5a7560
Campbell Lowman <campbelllowman@gmail.com>
2023-05-03 14:45:28 -0400

    Add a basic test with vitest

--- src/session/join-link/JoinLink.test.tsx
+++ src/session/join-link/JoinLink.test.tsx
@@ -1,3 +1,12 @@
-test('Renders main page correctly', () => {
-    expect(true).toBeTruthy();
-});
+import {describe, test, expect} from 'vitest';
+import { render, screen } from '@testing-library/react';
+import JoinLink from './JoinLink';
+
+describe("Join Link Test", () => {
+    test("Should render props", () => {
+        render(<JoinLink sessionID={123456} numberOfVoters={69} maximumVoters={420}/>)
+
+        expect(screen.getByText("Session 123456"));
+        expect(screen.getByText("69/420 Voters"));
+    })
+})
