4edb96b5b702affed89b2c318aa51468835698df
Campbell Lowman <campbelllowman@gmail.com>
2023-06-06 15:57:41 -0400

    debug

--- src/website/login/Login.tsx
+++ src/website/login/Login.tsx
@@ -13,6 +13,7 @@ const LOGIN = graphql(`
 
 function Login() {
     console.log(`graphql server: ${import.meta.env.VITE_GRAPHQL_HTTP_SERVER}`);
+    console.log(`subscription server: ${import.meta.env.VITE_GRAPHQL_WS_SERVER}`);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
