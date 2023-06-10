ccaea827a6fbbdcfaacdbf3a7f7f5cb1c9582d1d
Campbell Lowman <campbelllowman@gmail.com>
2023-06-10 16:19:52 -0400

    add welcome page body block

--- src/website/welcome/Welcome.tsx
+++ src/website/welcome/Welcome.tsx
@@ -5,6 +5,7 @@ import {createComponent} from '@lit-labs/react';
 import { MdFilledButton } from '@material/web/button/filled-button.js';
 import { MdOutlinedButton } from '@material/web/button/outlined-button';
 import { ReactComponent as LogoIcon }  from '../../assets/vectors/logo-icon.svg'
+import SessionMockup from '../../assets/images/session-mock-up.png'
 
 const MdFilledButtonComponent = createComponent({
     tagName: 'md-filled-button',
@@ -31,7 +32,20 @@ function Welcome() {
                     <MdOutlinedButtonComponent className='navigation-button' onClick={() => {navigate("/login");}}>Login</MdOutlinedButtonComponent>
                 </div>
             </div>
-            <div className='display-small'>Welcome</div>
+            <div className='body-block-1'>
+                <div className='body-block-1-text'>
+                    <h1 className='display-medium' style={{padding: '0.5rem'}}>Democratized DJing</h1>
+                    <p className='headline-small' style={{padding: '0.5rem'}}>Fazool allows a group of people to vote on what songs get played.</p>
+                    <ul style={{padding: '0.5rem'}}>
+                        <li className='title-medium' style={{listStylePosition: 'inside'}}>Connects with popular streaming services</li>
+                        <li className='title-medium' style={{listStylePosition: 'inside'}}>Basic functionality is completely free</li>
+                        <li className='title-medium' style={{listStylePosition: 'inside'}}>No account required for voters</li>
+                    </ul>
+                </div>
+                <div>
+                    <img className='session-mockup' src={SessionMockup} alt='Mock up of a Fazool session'/>
+                </div>
+            </div>
         </div>
     )
 }
