b50dc3056b3815602ddc7f4c4db21776080eba16
Campbell Lowman <campbelllowman@gmail.com>
2023-06-10 11:00:02 -0400

    home page header navigation

--- src/website/welcome/Welcome.tsx
+++ src/website/welcome/Welcome.tsx
@@ -1,27 +1,38 @@
 import './Welcome.css'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import React from 'react';
 import {createComponent} from '@lit-labs/react';
 import { MdFilledButton } from '@material/web/button/filled-button.js';
+import { MdOutlinedButton } from '@material/web/button/outlined-button';
+import { ReactComponent as LogoIcon }  from '../../assets/vectors/logo-icon.svg'
 
 const MdFilledButtonComponent = createComponent({
     tagName: 'md-filled-button',
     elementClass: MdFilledButton,
     react: React,
-  });
+});
+
+const MdOutlinedButtonComponent = createComponent({
+    tagName: 'md-outlined-button',
+    elementClass: MdOutlinedButton,
+    react: React,
+});
 
 function Welcome() {
+    const navigate = useNavigate();
+
     return (
-        <>
-            <div className='nav-bar'>
-                <div>Logo</div>
-                <Link className='nav-button' to='/join'>Join Session</Link>
-                <Link className='nav-button' to='/register'>Sign Up!</Link>
-                <Link className='nav-button' to='/login'>Login</Link>
-                <MdFilledButtonComponent onClick={() => {console.log("CLick")}}>test</MdFilledButtonComponent>
+        <div className='welcome-page'>
+            <div className='navigation-header'>
+                <LogoIcon className='logo-wrapper'/>
+                <div>
+                    <MdOutlinedButtonComponent className='navigation-button' onClick={() => {navigate("/join");}}>Join Session</MdOutlinedButtonComponent>
+                    <MdFilledButtonComponent className='navigation-button' onClick={() => {navigate("/register");}}>Sign Up</MdFilledButtonComponent>
+                    <MdOutlinedButtonComponent className='navigation-button' onClick={() => {navigate("/login");}}>Login</MdOutlinedButtonComponent>
+                </div>
             </div>
-            <div>Welcome</div>
-        </>
+            <div className='display-small'>Welcome</div>
+        </div>
     )
 }
 
