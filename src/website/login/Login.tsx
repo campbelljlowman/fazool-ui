04c4a80e78d4c3cb592fdca63600b1ccb4dcf640
Campbell Lowman <campbelllowman@gmail.com>
2023-06-13 20:15:05 -0400

    update login page

--- src/website/login/Login.tsx
+++ src/website/login/Login.tsx
@@ -3,7 +3,17 @@ import { useMutation } from '@apollo/client';
 import { useNavigate } from 'react-router-dom';
 import { graphql } from '../../gql';
 import { AccountLogin } from '../../gql/graphql';
+import { ReactComponent as LogoIcon }  from '../../assets/vectors/logo-icon.svg'
+import './Login.css'
+import React from 'react';
+import {createComponent} from '@lit-labs/react';
+import { MdFilledButton } from '@material/web/button/filled-button.js';
 
+const MdFilledButtonComponent = createComponent({
+    tagName: 'md-filled-button',
+    elementClass: MdFilledButton,
+    react: React,
+});
 
 const LOGIN = graphql(`
     mutation login ($accountLogin: AccountLogin!) {
@@ -31,13 +41,14 @@ function Login() {
         setPassword(e.target.value);
     }
 
-    const login = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
+    const login = (event: React.MouseEvent<MdFilledButton>) => {
+        event.preventDefault();
         const accountLogin: AccountLogin = {
             "email": email,
             "password": password
         };
 
+        console.log("Login values: " + JSON.stringify(accountLogin));
         loginMutation({ variables: { accountLogin: accountLogin } });
     }
 
@@ -45,19 +56,16 @@ function Login() {
     if (loginMutationError) console.log(`Error! ${loginMutationError.message}`)
 
     return (
-        <>
-            <div>Login</div>
-            <form>
-                <label>Email:<br />
-                    <input type="text" placeholder="Email" value={email} onChange={handleEmail} />
-                </label><br />
+        <div className='login-page'>
+                <LogoIcon className='logo-wrapper-main'/>
+                <div className='login-forms-card'> 
+                    <h1 className='display-small'>Log In</h1>
 
-                <label >Password:<br />
-                    <input type="password" placeholder="Password" value={password} onChange={handlePassword} />
-                </label><br />
-                <button className="transparent-button" onClick={login}>Login</button>
-            </form>
-        </>
+                    <input className='input-field' type="text" placeholder="Email" value={email} onChange={handleEmail} />
+                    <input className='input-field' type="password" placeholder="Password" value={password} onChange={handlePassword} />
+                    <MdFilledButtonComponent className='navigation-button' onClick={login}>Log In</MdFilledButtonComponent>   
+                </div>
+        </div>
     )
 }
 
