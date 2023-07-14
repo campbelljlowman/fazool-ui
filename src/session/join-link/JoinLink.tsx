e15f045d179c98abc0f6b906d484a02db93eada0
Campbell Lowman <campbelllowman@gmail.com>
2023-07-14 14:43:15 -0400

    small tweaks

--- src/session/join-link/JoinLink.tsx
+++ src/session/join-link/JoinLink.tsx
@@ -13,12 +13,12 @@ function JoinLink({ sessionID, numberOfVoters, maximumVoters}: JoinLinkProps) {
                 <p className='font-medium text-2xl'>{numberOfVoters}/{maximumVoters} Voters</p>
                 <p className='text-muted-foreground '>Session {sessionID}</p>
             </div>
-            <div className='flex flex-col w-1/2 mb-4'>
+            <div className='flex flex-col items-center w-1/2 mb-4'>
                     <AspectRatio ratio={1}>
                         <img className='rounded-lg h-full w-auto' src={JoinQRCode} alt='QR code link to fazool.party' />
                     </AspectRatio>
                 <p className='text-xl leading-6 mt-2'>fazool.us/join</p>
-                <p className='text-muted-foreground'>Free, no account required</p>
+                <p className='text-muted-foreground'>No account required</p>
             </div>
         </div>
     );
