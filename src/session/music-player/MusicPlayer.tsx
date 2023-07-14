081ca1f14b44e421744cb433ca8693549c928db1
Campbell Lowman <campbelllowman@gmail.com>
2023-07-14 15:31:58 -0400

    small tweak to currently playing

--- src/session/music-player/MusicPlayer.tsx
+++ src/session/music-player/MusicPlayer.tsx
@@ -19,7 +19,9 @@ function MusicPlayer({ sessionID, currentlyPlaying, showMediaButtons }: MusicPla
 
     return (
         <div className='flex flex-col justify-between items-center h-full' >
-            <Song song={currentlyPlaying.simpleSong} />
+            <div className='flex flex-col justify-center items-center h-full w-full'>
+                <Song song={currentlyPlaying.simpleSong} />
+            </div>
             <div className='flex flex-col w-full items-center gap-1'>
                 <MediaButtons showMediaButtons={showMediaButtons} currentlyPlaying={currentlyPlaying.isPlaying} sessionID={sessionID}/>
                 <ProgressBar isPlaying={currentlyPlaying.isPlaying} songProgress={currentlyPlaying.songProgressSeconds} songDuration={currentlyPlaying.songDurationSeconds}/>
