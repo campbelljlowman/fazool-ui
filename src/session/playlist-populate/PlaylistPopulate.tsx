2dafb61352a1100ef649a9d42d8a7d2910130c7b
Campbell Lowman <campbelllowman@gmail.com>
2023-07-17 13:14:55 -0700

    migrate playlist populate message

--- src/session/playlist-populate/PlaylistPopulate.tsx
+++ src/session/playlist-populate/PlaylistPopulate.tsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import './PlaylistPopulate.css'
 import { useLazyQuery } from '@apollo/client';
 import { graphql } from '../../gql';
 import PlaylistItem from './PlaylistItem';
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
 import { Button } from '@/components/ui/button';
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 
 const PLAYLIST_SEARCH  = graphql(`
     query playlists ($sessionID: Int!){
@@ -44,28 +36,22 @@ function PlaylistPopulate({ sessionID, isAdmin }: PlaylistPopulateProps) {
     if(playlistSearchQueryError) return <div>Error getting playlists: {playlistSearchQueryError.message}</div>
 
     if(!playlistSearchQueryData || !playlistSearchQueryData.playlists) {
-        // if (isAdmin) {
         return (
             <div className='flex flex-col justify-center items-center h-[92vh]'>
-                <Card className='flex flex-col w-1/4 items-center'>
-                    <CardHeader>
-                        <CardTitle>Queue is empty</CardTitle>
-                    </CardHeader>
-                    <CardContent className='text-center'>
-                        <p>Add songs to the queue with the search bar below</p>
-                        {isAdmin && <p>Or you can populate the queue with songs from a playlist</p>}
-                    </CardContent>
-                    {isAdmin && <CardFooter>
-                        <Button onClick={searchForPlaylists}>Search Playlists</Button>
-                    </CardFooter>}
-                </Card>
+                <Alert className='flex justify-between items-center w-1/2'>
+                    <div>
+                        <AlertTitle>
+                            Queue is empty!
+                        </AlertTitle>
+                        <AlertDescription>
+                            {isAdmin && <p>Populate the queue with songs from a playlist</p>}
+                            {!isAdmin && <p>Add songs to the queue with the search bar below</p>}
+                        </AlertDescription>
+                    </div>
+                    {isAdmin && <Button onClick={searchForPlaylists}>Search Playlists</Button>}
+                </Alert>
             </div>
         )
-        // } else {
-        //     return (
-        //         <div className='empty-queue-message'>Looks like the queue is empty. Add songs with the search bar</div>
-        //     )
-        // }
     }
 
     return (
