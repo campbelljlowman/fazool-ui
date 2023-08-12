e816651bb9d080ee6a8051a083ab1b876392c71c
Campbell Lowman <campbelllowman@gmail.com>
2023-08-12 11:48:41 -0400

    Make phone number optional

--- src/website/register/Register.tsx
+++ src/website/register/Register.tsx
@@ -22,7 +22,8 @@ const formSchema = z.object({
     firstName: z.string().min(1, { message: "First name is required" }),
     lastName: z.string().min(1, { message: "Last name is required" }),
     email: z.string().email(),
-    phoneNumber: z.string().refine(validator.isMobilePhone),
+    phoneNumber: z.union([z.string().length(0, {message: "Must be valid phone number"}), z.string().refine(validator.isMobilePhone)]).optional(),
+    // phoneNumber: z.string().refine(validator.isMobilePhone),
     password: z.string().min(1, { message: "Password is required" })
 })
 
